dcefb514eb972cea82636183bc145e6a6876393a
luiggy macias <ferrinluiggy@gmail.com>
2022-01-06 08:48:20 -0500

    son of a

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,9 @@ import indexRoute from "./routes/index.route";
 import cors from "cors";
 const app = express();
 
+// set engine
+app.set("view engine", "ejs");
+
 // middlewares
 app.use(express.json());
 app.use(cors()); //nunca te olvides de poner esto, si es que vas a usar las api de otro lado, osea de otro dominio o proxy
--- backend/src/routes/index.route.ts
+++ backend/src/routes/index.route.ts
@@ -3,5 +3,16 @@ import { hello } from "../controllers/index.controller";
 const router = Router();
 
 router.route("/api/hello").get(hello);
-router.route("login").get().post();
+router.route("/").get((req, res) => {
+  // si ya iniciamos mostrar bienvenida
+  // Si no hermos iniciado sesion redireccionar a /login
+});
+router
+  .route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+    res.send("hola mundo");
+  });
 export default router;
